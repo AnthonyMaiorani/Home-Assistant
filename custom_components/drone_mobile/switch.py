8f943f5e41489f930c0b2b02ee4f2b0b3c1baa0e
Anthony <maiorania@gmail.com>
2022-08-20 02:45:34 -0500

    automation improvements

--- custom_components/drone_mobile/switch.py
+++ custom_components/drone_mobile/switch.py
@@ -11,37 +11,35 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Add the Switch Entities from the config."""
     entry = hass.data[DOMAIN][config_entry.entry_id]
-    switches = []
     for key, value in SWITCHES.items():
-        async_add_entities([Switch(entry, key)], True)
+        async_add_entities([Switch(entry, key, config_entry.options)], True)
 
 
 class Switch(DroneMobileEntity, SwitchEntity):
-    def __init__(self, coordinator, switch):
+    def __init__(self, coordinator, switch, options):
         """Initialize."""
-        super().__init__(
-            device_id="dronemobile_" + switch,
-            name=coordinator.data["vehicle_name"] + "_" + switch,
-            coordinator=coordinator,
-        )
-        self._switch = switch
+        self._device_id="dronemobile_" + switch
+        self.coordinator=coordinator
+        self.switch = switch
         self._state = self.is_on
+        # Required for HA 2022.7
+        self.coordinator_context = object()
 
     async def async_turn_on(self, **kwargs):
         """switches on the vehicle device."""
         if self.is_on:
             return
         _LOGGER.debug(
-            "switching on %s " + self._switch, self.coordinator.data["vehicle_name"]
+            "switching on %s " + self.switch, self.coordinator.data["vehicle_name"]
         )
         command_call = None
-        if self._switch == "remoteStart":
+        if self.switch == "remoteStart":
             command_call = self.coordinator.vehicle.start
-        elif self._switch == "panic":
+        elif self.switch == "panic":
             command_call = self.coordinator.vehicle.panic_on
-        elif self._switch == "aux1":
+        elif self.switch == "aux1":
             command_call = self.coordinator.vehicle.aux1
-        elif self._switch == "aux2":
+        elif self.switch == "aux2":
             command_call = self.coordinator.vehicle.aux2
         else:
             return
@@ -59,12 +57,12 @@ class Switch(DroneMobileEntity, SwitchEntity):
         if not self.is_on:
             return
         _LOGGER.debug(
-            "Switching off %s " + self._switch, self.coordinator.data["vehicle_name"]
+            "Switching off %s " + self.switch, self.coordinator.data["vehicle_name"]
         )
         command_call = None
-        if self._switch == "remoteStart":
+        if self.switch == "remoteStart":
             command_call = self.coordinator.vehicle.stop
-        elif self._switch == "panic":
+        elif self.switch == "panic":
             command_call = self.coordinator.vehicle.panic_off
         else:
             return
@@ -80,22 +78,22 @@ class Switch(DroneMobileEntity, SwitchEntity):
     async def async_toggle(self, **kwargs):
         """Toggles the vehicle switch."""
         _LOGGER.debug(
-            "Toggling %s " + self._switch, self.coordinator.data["vehicle_name"]
+            "Toggling %s " + self.switch, self.coordinator.data["vehicle_name"]
         )
         command_call = None
-        if self._switch == "remoteStart":
+        if self.switch == "remoteStart":
             if self.is_on:
                 command_call = self.coordinator.vehicle.remoteStop
             else:
                 command_call = self.coordinator.vehicle.remoteStart
-        elif self._switch == "panic":
+        elif self.switch == "panic":
             if self.is_on:
                 command_call = self.coordinator.vehicle.panic_off
             else:
                 command_call = self.coordinator.vehicle.panic_on
-        elif self._switch == "aux1":
+        elif self.switch == "aux1":
             command_call = self.coordinator.vehicle.aux1
-        elif self._switch == "aux2":
+        elif self.switch == "aux2":
             command_call = self.coordinator.vehicle.aux2
             return
         response = await self.coordinator.hass.async_add_executor_job(
@@ -106,11 +104,19 @@ class Switch(DroneMobileEntity, SwitchEntity):
         )
         self._state = self.get_is_on_value(True)
         self.async_write_ha_state()
+    
+    @property
+    def name(self):
+        return self.coordinator.data["vehicle_name"] + "_" + self.switch + "_Switch"
 
+    @property
+    def device_id(self):
+        return self.device_id
+        
     # Need to remove @property decorator in order to be able to change logic based on where the call to is_on is made from.
     def get_is_on_value(self, calledFromAction=False):
         """Determine if the switch is switched."""
-        if self._switch == "remoteStart":
+        if self.switch == "remoteStart":
             if (
                 self.coordinator.data is None
                 or self.coordinator.data["last_known_state"]["controller"]["engine_on"]
@@ -128,7 +134,7 @@ class Switch(DroneMobileEntity, SwitchEntity):
                     ]
                     == True
                 )
-        elif self._switch == "panic":
+        elif self.switch == "panic":
             if (
                 self.coordinator.data is None
                 or self.coordinator.data["panic_status"] is None
@@ -136,15 +142,15 @@ class Switch(DroneMobileEntity, SwitchEntity):
                 return None
             return self.coordinator.data["panic_status"] == True
         # Aux1 and Aux2 are momentary switches that can only be turned on. So, we will set their value to off.
-        elif self._switch == "aux1":
+        elif self.switch == "aux1":
             return False
-        elif self._switch == "aux2":
+        elif self.switch == "aux2":
             return False
         else:
-            _LOGGER.error("Entry not found in SWITCHES: " + self._switch)
+            _LOGGER.error("Entry not found in SWITCHES: " + self.switch)
 
     is_on = property(get_is_on_value)
 
     @property
     def icon(self):
-        return SWITCHES[self._switch]["icon"]
+        return SWITCHES[self.switch]["icon"]
