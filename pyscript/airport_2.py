6c4199eb0023a560d165fdc1817f80234d6ff62d
Anthony <maiorania@gmail.com>
2023-04-02 03:47:02 -0500

    changes

---
+++ pyscript/airport_2.py
@@ -0,0 +1,19 @@
+import aiohttp
+import plistlib
+
+state.persist("pyscript.airport_2_playing")
+
+@time_trigger("period(2020/01/01, 5sec)")
+async def get_giga_state():
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get("http://192.168.1.44:7000/info") as res:
+                data = res.read()  # requires await outside HA pyscript environemnt
+                plistData = plistlib.loads(data)
+
+                playing = plistData["statusFlags"] & 0x800 != 0
+
+                pyscript.airport_2_playing = playing
+
+        except Exception as e:
+            pyscript.airport_2_playing = "Error: " + repr(e)
